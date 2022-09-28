d64f036b10217dfc745e03ca245e80451bafae61
Fernando Cossio <39391180+fcossio@users.noreply.github.com>
2022-09-28 10:03:11 +0200

    Create useful_bash_commands.sh

---
+++ useful_bash_commands.sh
@@ -0,0 +1,3 @@
+# Run a command repeateadly until success from https://stackoverflow.com/questions/5274294/how-can-you-run-a-command-in-bash-over-and-over-until-success
+until <my command>; do echo "Try again"; sleep 2; done
+
