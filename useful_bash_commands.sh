005c834559c2affd5bae9b4276ed5d19309caa0a
Fernando Cossio <39391180+fcossio@users.noreply.github.com>
2022-09-28 10:04:53 +0200

    Update useful_bash_commands.sh

--- useful_bash_commands.sh
+++ useful_bash_commands.sh
@@ -1,3 +1,5 @@
+# This is my useful commands cache.
+
 # Run a command repeateadly until success from https://stackoverflow.com/questions/5274294/how-can-you-run-a-command-in-bash-over-and-over-until-success
 until <my command>; do echo "Try again"; sleep 2; done
 
