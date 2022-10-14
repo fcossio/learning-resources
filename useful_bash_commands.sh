35eed3ba95f1afc09c700bad0bc564e5be9daefd
Fernando Cossio <39391180+fcossio@users.noreply.github.com>
2022-10-14 10:28:44 +0200

    Update useful_bash_commands.sh

--- useful_bash_commands.sh
+++ useful_bash_commands.sh
@@ -3,3 +3,5 @@
 # Run a command repeateadly until success from https://stackoverflow.com/questions/5274294/how-can-you-run-a-command-in-bash-over-and-over-until-success
 until <my command>; do echo "Try again"; sleep 2; done
 
+# Mount a SMB share
+sudo mount -t cifs -o username=<username>,dir_mode=0<dirmode eg 777>,file_mode=0<filemode eg 777> //<ip>/<subpath>/ <mount path>
