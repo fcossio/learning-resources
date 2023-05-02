41f0649db122b4df4262217593eb3fa5bb417aca
Fernando Cossio <39391180+fcossio@users.noreply.github.com>
2023-05-02 16:59:32 +0200

    Update useful_bash_commands.sh

--- useful_bash_commands.sh
+++ useful_bash_commands.sh
@@ -11,3 +11,6 @@ sudo mount -o id=$(id) /dev/sdxx /folder
 
 # Get byte size for a list of filepaths
 cat list_of_fp.txt | xargs -I % wc -c % >> file_sizes.txt
+
+# Download a tar and uncompress at the same time
+wget -c <url>.tar.gz -O - | tar -xz
